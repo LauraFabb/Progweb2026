6c92e2f28ea5ff26dee9326e4d2b0a067afb7392
LauraFabb <laura.fabbri@heig-vd.ch>
2026-09-22 11:54:29 +0200

    ex4 début

--- Cours-1/Exercices.js
+++ Cours-1/Exercices.js
@@ -17,12 +17,12 @@ function value(a, b, c) {
 console.log(value(5, 8, 2));
 
 //ex2 Ecrire une fonction qui retourne un nombre entier pseudo-aléatoire entre une borne inférieure et une borne supérieure (bornes entières et comprises dans l'intervalle).
-function getRandomInt(min, max){
-return (Math.random() + min) * (max - min);
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (let i=0;i>10;i++){
-
+for (let i = 0; i < 8; i++) {
+    console.log(getRandomInt(4, 8));
 }
 
 //ex3  Ecrire deux fonctions compareA et compareB qui retournent les mêmes résultats que dans les exemples suivant:
@@ -40,3 +40,40 @@ console.log(compareA(4, 'quatre'));
 
 console.log(compareB(8, '8'));
 console.log(compareB(8, 'huit'));
+
+
+//ex4 En fonction d'un nombre n (ou n > 0) donné en paramètre, écrire une fonction qui affiche dans la console :
+
+   
+   // Les nombres entiers pairs et multiples de 7 compris entre 0 et n.
+   function getEvenSeven(n){
+    for (let even = 0; even <= n ; even = even +2){
+      if  (even % 7 == 0)
+      
+console.log(even);
+      }
+    }
+
+    getEvenSeven(72);
+   
+    //Les nombres entiers pairs et multiples de 3, ainsi que les nombres entiers multiple de 7 compris entre 0 et n.
+
+    function getEvenConditions(n){
+      for (let even = 0 ; even <= n ; even = even +2){
+        if (even % 7 == 0)
+
+          console.log (even);
+      }
+    }
+
+    getEvenConditions(getEven);
+   // Les nombres entiers pairs et multiples de 3, mais non multiples de 7 compris entre 0 et n.
+
+   //Les nombres entiers pairs compris entre 0 et n.
+   function getEven(n){
+     for (let even = 0 ;even <=n ; even = even + 2){
+      console.log (even);
+     }
+   }
+
+   getEven(19);
